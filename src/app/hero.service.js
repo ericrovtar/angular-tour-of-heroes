42b51c84d3e6f07adb9f5b0557a5f5abf6013477
Eric Rovtar <eric.rovtar@me.com>
2017-06-07 10:58:20 -0400

    Add delete method and service

--- src/app/hero.service.js
+++ src/app/hero.service.js
@@ -30,12 +30,27 @@ var HeroService = (function () {
             .then(function (response) { return response.json().data; })
             .catch(this.handleError);
     };
+    HeroService.prototype.create = function (name) {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
     HeroService.prototype.update = function (hero) {
         var url = this.heroesUrl + "/" + hero.id;
         return this.http
             .put(url, JSON.stringify(hero), { headers: this.headers })
             .toPromise()
             .then(function () { return hero; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.delete = function (id) {
+        var url = this.heroesUrl + "/" + id;
+        return this.http
+            .delete(url, { headers: this.headers })
+            .toPromise()
+            .then(function () { return null; })
             .catch(this.handleError);
     };
     HeroService.prototype.handleError = function (error) {
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -28,6 +28,14 @@ export class HeroService {
             .catch(this.handleError);
     }
 
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+    }
+
     update(hero: Hero): Promise<Hero> {
         const url = `${this.heroesUrl}/${hero.id}`;
 
@@ -35,6 +43,16 @@ export class HeroService {
             .put(url, JSON.stringify(hero), { headers: this.headers })
             .toPromise()
             .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+
+        return this.http
+            .delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => null)
             .catch(this.handleError);
     }
 
