b7134c29e21d5cda34a925f9077eb0d6979fb3ea
Eric Rovtar <eric.rovtar@me.com>
2017-06-07 09:21:06 -0400

    Abstract routing to its own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -11,30 +11,11 @@ import { HeroService } from './hero.service';
 
 import { DashboardComponent } from './dashboard.component.js'
 
-
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
